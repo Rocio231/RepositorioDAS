c1dbdacb45bca13e7f48cdd27ac3eb2c2d3b8244
Alejandro Moreno <84947921+XandroMrn@users.noreply.github.com>
2023-03-30 20:24:57 -0600

    Inserts ocupacion
    Inserts de la tabla de ocupacion
    
    Signed-off-by: Alejandro Moreno <84947921+XandroMrn@users.noreply.github.com>

---
+++ (Inserts) ocupacion.sql
@@ -0,0 +1,15 @@
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (1, 'Profesionista');
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (2, 'Tecnicos');
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (3, 'Trabajadores de la educacion');
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (4, 'Trabajadores del arte');
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (5, 'Funcionarios publicos');
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (6, 'Funcionarios privados');
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (7, 'Trabajador agricola');
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (8, 'Artesano');
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (9, 'Operador');
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (10, 'Conductor');
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (11, 'Supervisor');
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (12, 'Comerciante');
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (13, 'Vendedor ambulante');
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (14, 'Proteccion');
+INSERT INTO `db_censo`.`ocupacion` (`idocupacion`, `nombre_ocupacion`) VALUES (15, 'No especificado');
